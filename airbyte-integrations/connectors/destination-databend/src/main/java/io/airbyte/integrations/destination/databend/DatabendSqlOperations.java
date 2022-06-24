8eee75065185e53cd7d9032ced4c401041164ea0
zhihanz <zzh243402448@gmail.com>
2022-06-24 18:51:42 +0800

    save

--- airbyte-integrations/connectors/destination-databend/src/main/java/io/airbyte/integrations/destination/databend/DatabendSqlOperations.java
+++ airbyte-integrations/connectors/destination-databend/src/main/java/io/airbyte/integrations/destination/databend/DatabendSqlOperations.java
@@ -4,15 +4,23 @@
 
 package io.airbyte.integrations.destination.databend;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.airbyte.commons.json.Jsons;
 import io.airbyte.db.jdbc.JdbcDatabase;
 import io.airbyte.integrations.base.JavaBaseConstants;
 import io.airbyte.integrations.destination.jdbc.JdbcSqlOperations;
 import io.airbyte.protocol.models.AirbyteRecordMessage;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.yandex.clickhouse.ClickHouseConnection;
@@ -38,7 +46,7 @@ public class DatabendSqlOperations extends JdbcSqlOperations {
     return String.format(
         "CREATE TABLE IF NOT EXISTS %s.%s ( \n"
             + "%s String,\n"
-            + "%s String,\n"
+            + "%s JSON,\n"
             + "%s Date DEFAULT now(),\n"
             + ")\n"
             + "CLUSTER BY(%s)",
@@ -58,6 +66,25 @@ public class DatabendSqlOperations extends JdbcSqlOperations {
     database.execute(appendedQueries.toString());
   }
 
+  protected JsonNode formatData(final JsonNode data) {
+    return data;
+  }
+
+  protected void writeBatchToFileJSONEACHROW(final File tmpFile, final List<AirbyteRecordMessage> records) throws Exception {
+    try (final PrintWriter writer = new PrintWriter(tmpFile, StandardCharsets.UTF_8)) {
+      for (final AirbyteRecordMessage record : records) {
+        final var uuid = UUID.randomUUID().toString();
+        final var jsonData = Jsons.serialize(formatData(record.getData()));
+        final var emittedAt = Timestamp.from(Instant.ofEpochMilli(record.getEmittedAt()));
+        JSONObject object = new JSONObject();
+        object.put(JavaBaseConstants.COLUMN_NAME_AB_ID,uuid);
+        object.put(JavaBaseConstants.COLUMN_NAME_DATA, jsonData);
+        object.put(JavaBaseConstants.COLUMN_NAME_EMITTED_AT, emittedAt.toString());
+        writer.print(object);
+        writer.println();
+      }
+    }
+  }
   @Override
   public void insertRecordsInternal(final JdbcDatabase database,
                                     final List<AirbyteRecordMessage> records,
@@ -75,13 +102,13 @@ public class DatabendSqlOperations extends JdbcSqlOperations {
       Exception primaryException = null;
       try {
         tmpFile = Files.createTempFile(tmpTableName + "-", ".tmp").toFile();
-        writeBatchToFile(tmpFile, records);
+        writeBatchToFileJSONEACHROW(tmpFile, records);
 
         ClickHouseConnection conn = connection.unwrap(ClickHouseConnection.class);
         ClickHouseStatement sth = conn.createStatement();
         sth.write() // Write API entrypoint
             .table(String.format("%s.%s", schemaName, tmpTableName)) // where to write data
-            .data(tmpFile, ClickHouseFormat.CSV) // specify input
+            .data(tmpFile, ClickHouseFormat.JSONEachRow) // specify input
             .send();
 
       } catch (final Exception e) {
